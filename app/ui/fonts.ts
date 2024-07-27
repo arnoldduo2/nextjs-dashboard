8d32d1058d1385f47dd8d56cef6cd2b5f654b523
arnoldduo2 <arnoldduo2@gmail.com>
2024-07-27 17:48:02 +0200

    NextJs-app init

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Lusitana, Maven_Pro } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ weight: '400', subsets: ['latin'] });
+export const maven = Maven_Pro({ subsets: ['latin'] });
